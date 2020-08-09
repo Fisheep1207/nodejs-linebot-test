f981c80d465416f34e7fbe6191ebe1b9ca7bb8c3
Fisheep1207 <qiuyuyang@Fisheep.local>
2020-08-10 02:06:33 +0800

    second

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-const line = require('@line/bot-sdk');
-const express = require('express');
-const lineConfig = {
-  channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
-};
-const client = new line.Client(lineConfig);
-const app = express();
-app.listen(3000, function() {
-    console.log('App now running on port', this.address().port);
+let linebot = require('linebot');
+
+// 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars，可參考 Step2
+let bot = linebot({
+  channelId: process.env.LINE_CHANNEL_ID,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
-app.post('/', line.middleware(lineConfig), function(req, res) {
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then(function(result) {
-        res.json(result);
-      });
+// 當有人傳送訊息給 Bot 時
+bot.on('message', function (event) {
+  // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
+  event.reply(`你說了 ${event.message.text}`);
 });
 
-function handleEvent(event) {
-    switch (event.type) {
-      case 'join':
-      case 'follow':
-        return client.replyMessage(event.replyToken, {
-          type: 'text',
-          text: '你好請問我們認識嗎?'
-        });   
-      case 'message':
-        switch (event.message.type) {
-          case 'text':
-            return client.replyMessage(event.replyToken, {
-              type: 'text',
-              text: (event.message.text+'~*')
-            });
-        }
-    }
-}
+// Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
+bot.listen('/', process.env.PORT || 5000, function () {
+  console.log('全國首家LINE線上機器人上線啦！！');
+});
