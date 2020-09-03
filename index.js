7210cfd53d85e916fd5a57ba3b30e1d64589213c
Fisheep1207 <qiuyuyang@Fisheep.local>
2020-09-04 00:05:06 +0800

    multi messages

--- index.js
+++ index.js
@@ -7,47 +7,35 @@ let bot = linebot({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
-var members = {}
+let members = {}
 
 // 當有人傳送訊息給 Bot 時
 bot.on('message', function (event) {
   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
-  event.reply("WTF");
-  event.reply(event.message.text);
-  // let mes = event.message.text;
-  // if (mes == undefined){
-  //   event.reply("我看不懂 :(((( ");
-  // }
-  // else{
-  //   let id = event.source.userId;
-  //   console.log("pls: ", mes == "say");
-  //   if (mes == "say"){
-  //     console.log(`members = ${members}`)
-  //     event.reply("進來了！")
-  //     // for(let i = 1 ; i < members[id].length; i ++){
-  //     //   event.reply(`${members[id][i]}`);
-  //     // }
-  //   }
-  //   else{
-  //     console.log("here1");
-  //     if(id in members){
-  //       console.log("here2");
-  //       if (members[id].length <= 6){
-  //         console.log("here3");
-  //         members[id].push(mes)
-  //       }
-  //       else{
-  //         console.log("here4");
-  //         members[id][members[id][0]%5 + 1] = mes
-  //         members[id][0]++;
-  //       }
-  //     }
-  //     else{
-  //       console.log("here5");
-  //       members[id] = [1, mes]
-  //     }
-  //   }
-  // }
+  let mes = event.message.text;
+  if (mes == undefined){
+    event.reply("我看不懂 :(((( ");
+  }
+  else{
+    let id = event.source.userId;
+    if (mes == "ghost"){
+      event.reply(members[id]);
+    }
+    else{
+      if(id in members){
+        if(members[id].length <= 5){
+          members[id].push();
+        }
+        else{
+          members[id].shift();
+          members[id].push();
+        }
+      }
+      else{
+        members[id] = [mes]
+      }
+    }
+  }
 });
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
