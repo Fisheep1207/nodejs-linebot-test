cf7ddd28abb7bef3578c8168c57c48dc21b51770
Fisheep1207 <qiuyuyang@Fisheep.local>
2020-09-04 11:51:18 +0800

    debug push without param

--- index.js
+++ index.js
@@ -7,39 +7,30 @@ let bot = linebot({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
-var members = {}
+let members = {}
 
 // 當有人傳送訊息給 Bot 時
-bot.on('message', async function (event) {
+bot.on('message', function (event) {
   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
   console.log(members)
   let mes = event.message.text;
   let id = event.source.userId;
-  console.log(`mes = ${mes}, id = ${id}`);
-  if(id in members){
-    console.log("here1");
-    if(members[id].length <= 5){
-      console.log("here2");
-      members[id].push();
-      console.log("here3");
-    }
-    else{
-      console.log("here4");
-      members[id].shift();
-      console.log("here5");
-      members[id].push();
-      console.log("here6");
-    }
-    console.log("here7");
-    console.log(members);
-    await event.reply(members[id]);
-    console.log("here8");
+  if (mes == "ghost"){
+    event.reply(members[id]);
   }
   else{
-    console.log("Nononononon");
-    members[id] = [mes];
-    console.log("here end");
-    return;
+    if(id in members){
+      if(members[id].length <= 5){
+        members[id].push(mes);
+      }
+      else{
+        members[id].shift();
+        members[id].push(mes);
+      }
+    }
+    else{
+      members[id] = [mes]
+    }
   }
 });
 
